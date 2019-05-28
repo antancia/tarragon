88c8dd2f6c42208d5d46064f3dc4dd2881d90f55
Harmony <antancia@gmail.com>
2019-05-27 17:38:57 -0700

    ingredient list wip

--- src/components/RecipeTemplate.js
+++ src/components/RecipeTemplate.js
@@ -1,29 +1,75 @@
-import React, { useState } from 'react';
-import { Input, Form, FormGroup, Label } from 'reactstrap';
+import React, { Fragment, useState } from 'react';
+import {
+  Card,
+  CardBody,
+  CardText,
+  CardTitle,
+  Input,
+  Form,
+  FormGroup,
+  Label,
+} from 'reactstrap';
 
 export default function RecipeTemplate() {
   const [title, setTitle] = useState('');
+  const [ingredients, setIngredients] = useState([
+    { name: 'Lemon', checked: false },
+  ]);
 
-  const onSubmitTitle = e => {
-    e.preventDefault();
-    setTitle(e.target.value);
+  const checkIngredient = i => {
+    const copy = [...ingredients];
+    const ingredient = copy[i];
+    copy[i] = { ...ingredient, checked: !ingredient.checked };
+    setIngredients(copy);
   };
 
+  const addIngredient = name => {
+    const copy = [...ingredients];
+    copy.push({ name, checked: false });
+    setIngredients(copy);
+  };
+
+  const IngredientChecklist = () =>
+    ingredients.map((ingredient, i) => (
+      <Fragment>
+        <Input
+          type="checkbox"
+          id={`ingredient-${i}`}
+          checked={ingredient.checked}
+          onChange={() => checkIngredient(i)}
+        />
+        <Label for={`ingredient-${i}`} check>
+          <CardText>{ingredient.name}</CardText>
+        </Label>
+      </Fragment>
+    ));
+
+  const IngredientsCard = () => (
+    <Card>
+      <CardTitle>Ingredients</CardTitle>
+      <CardBody>
+        <FormGroup check>
+          <IngredientChecklist />
+        </FormGroup>
+      </CardBody>
+    </Card>
+  );
+
   return (
-    <div>
+    <Fragment>
       <Form onSubmit={e => e.preventDefault()}>
         <FormGroup>
           <Input
             type="text"
-            name="title"
-            id="title"
+            id="recipe-title"
             placeholder="Untitled"
             className="form-control-lg"
             value={title}
-            onChange={onSubmitTitle}
+            onChange={e => setTitle(e.targetValue)}
           />
         </FormGroup>
+        <IngredientsCard />
       </Form>
-    </div>
+    </Fragment>
   );
 }
